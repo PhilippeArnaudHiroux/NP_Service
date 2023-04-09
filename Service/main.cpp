fe923358fdd0caba03bd64bdc440fa416668fd7c
Philippe-Arnaud Hiroux <12002903@student.pxl.be>
2023-04-09 11:03:45 +0200

    cleaned service

--- Service/main.cpp
+++ Service/main.cpp
@@ -12,75 +12,70 @@
 
 using namespace std;
 
-string remove(string removeFrom);
-string takeFirstThree(string removeFrom);
-
 int main( void )
 {
-    string gekregenString = "";
-    string firstThree  ="";
+    string receivedString = ""; //In this string the received messages will be stored
+    string sendString = "";     //In this string the messages will be stored that will be send
+    string firstThree  ="";     //In this string will be the command stored
+    string theProduct = "";     //In this string will be the product stored
+    vector <string> shopBag;    //In this vector the product will be saved
     int i = 0;
-
-    vector <string> winkelManje;
 
     try
     {
         zmq::context_t context(1);
 
         //Incoming messages come in here
-        zmq::socket_t subscriber( context, ZMQ_SUB );                   //Service sub
-        zmq::socket_t ventilator( context, ZMQ_PUSH );                  //Service push
-        subscriber.connect( "tcp://benternet.pxl-ea-ict.be:24042" );    //Service sub
-        ventilator.connect( "tcp://benternet.pxl-ea-ict.be:24041" );    //Service push
-        subscriber.setsockopt( ZMQ_SUBSCRIBE, "pah", strlen("pah"));    //Service sub
+        zmq::socket_t subscriber( context, ZMQ_SUB );                       //Service sub
+        zmq::socket_t ventilator( context, ZMQ_PUSH );                      //Service push
+        subscriber.connect( "tcp://benternet.pxl-ea-ict.be:24042" );        //Service sub
+        ventilator.connect( "tcp://benternet.pxl-ea-ict.be:24041" );        //Service push
+        subscriber.setsockopt( ZMQ_SUBSCRIBE, "shop!", strlen("shop!"));    //Service sub
 
         zmq::message_t * msg = new zmq::message_t();
         while( subscriber.connected() && ventilator.connected() )
         {
-            subscriber.recv( msg );                                     //Ontvang het bericht
-            gekregenString = string((char*) msg->data(), msg->size());  //Vorm ontvangen data om naar een string
-            gekregenString = remove(gekregenString);                    //Verwijder de eerste 3 karakters van de string (sub topic)
-            firstThree = takeFirstThree(gekregenString);
+            subscriber.recv(msg);                                       //Receive the message
+            receivedString = string((char*) msg->data(), msg->size());  //Convert the received message to a string
+            receivedString.erase(0,5);                                  //Remove the first 5 characters of the string (sub topic)
+            firstThree = receivedString;                                //Set firstThree equal to receivedString
+            theProduct = receivedString;                                //Set theProduct equal to receivedString
+            firstThree.erase(3,firstThree.size());                      //Remove everything after the first 3 characters
+            theProduct.erase(0, 3);                                     //Remove the first 3 characters
 
-            if(firstThree == "add")                                                    //Als er add moet gebeuren
+            if(firstThree == "add")                                                 //If add
             {
-                gekregenString = remove(gekregenString);                                    //Verwijder 3 eerste karakters
-                cout << "add " << gekregenString << endl;                                   //Print het commando uit
-                winkelManje.push_back(gekregenString);                                      //Voeg het product toe aan het winkelmandje
-                gekregenString = "hpa" + gekregenString + " is toegevoegd aan uw mandje!";  //Stel de string samen die terugestuurd wordt
-                ventilator.send(gekregenString.c_str(), gekregenString.size());             //Stuur de string terug
-                //ventilator.send("hpaend", strlen("hpaend"));                                //Stuur door dat client mag stoppen met luisteren
+                cout << "add " << theProduct << endl;                               //Print out the text
+                shopBag.push_back(theProduct);                                      //Add the product to the shopBag
+                sendString = "hpa" + theProduct + " is toegevoegd aan uw mandje!";  //Create the string that will be send back
+                ventilator.send(sendString.c_str(), sendString.size());             //Send the string
             }
-            else if(firstThree == "get")                                       //Als er get moet gebeuren
+            else if(firstThree == "get")                                    //If get
             {
-                cout << "get winkelmanje" << endl;                                  //Print het commando uit
-                for(int j=0; j<winkelManje.size(); j++)                             //Doe dit evenvaak als de vector groot is
+                cout << "get winkelmanje" << endl;                          //Print out the text
+                for(int j=0; j<shopBag.size(); j++)                         //Do this as many times as the vector is large
                 {
-                    gekregenString = winkelManje.at(j);                             //Neem het element uit de vector kopieer het in gekregenString
-                    gekregenString = "hpa" + gekregenString;                        //Stel de string samen die tergestuurd wordt
-                    ventilator.send(gekregenString.c_str(), gekregenString.size()); //Stuur se string door
+                    theProduct = shopBag.at(j);                             //Take the element from the vector and copy it into theProduct
+                    sendString = "hpa" + theProduct;                        //Create the string that will be send back
+                    ventilator.send(sendString.c_str(), sendString.size()); //Send the string
                 }
-                //ventilator.send("hpaend", strlen("hpaend"));                        //Stuur door dat client mag stoppen met luisteren
             }
-            else if(firstThree == "del")                                   //Als er del moet gebeuren
+            else if(firstThree == "del")                                //If del
             {
-                gekregenString = remove(gekregenString);                        //Verwijder 3 eerste karakters
-                cout << "del " << gekregenString << endl;                       //Print het commando uit
-                while(gekregenString != winkelManje.at(i)){i++;}                //Kijk op welke posities het element staat in de vector
-                winkelManje.erase(winkelManje.begin()+i);                       //Verwijder het element op positie i in de vector
-                gekregenString = "hpa" + gekregenString + " is verwijderd";     //Stel de string samen
-                ventilator.send(gekregenString.c_str(), gekregenString.size()); //Stuur se string door
-                //ventilator.send("hpaend", strlen("hpaend"));                    //Stuur door dat client mag stoppen met luisteren
+                cout << "del " << theProduct << endl;                   //Print out the text
+                while(theProduct != shopBag.at(i)){i++;}                //Look at which positions the element is in the vector
+                shopBag.erase(shopBag.begin()+i);                       //Remove the element at position i in the vector
+                sendString = "hpa" + theProduct + " is verwijderd";     //Create the string that will be send back
+                ventilator.send(sendString.c_str(), sendString.size()); //Send the string
             }
             else
             {
-                gekregenString = takeFirstThree(gekregenString);
-                cout << gekregenString << " is geen commando" << endl;
-                gekregenString = "hpa" + gekregenString + " onbekend comando";     //Stel de string samen
-                ventilator.send(gekregenString.c_str(), gekregenString.size()); //Stuur se string door
-                //ventilator.send("hpaend", strlen("hpaend"));                    //Stuur door dat client mag stoppen met luisteren
+                cout << firstThree << " is geen commando" << endl;      //Print out the text
+                sendString = "hpa" + firstThree + " onbekend comando";  //Create the string that will be send back
+                ventilator.send(sendString.c_str(), sendString.size()); //Send the string
             }
-            ventilator.send("hpaend", strlen("hpaend"));                    //Stuur door dat client mag stoppen met luisteren
+
+            ventilator.send("hpaend", strlen("hpaend"));    //Forward that client may stop listening
         }
     }
     catch( zmq::error_t & ex )
@@ -89,16 +84,4 @@ int main( void )
     }
 
     return 0;
-}
-
-string remove(string removeFrom) //Een fuctie waarmee steeds de eerste 3 karakters van een string worden verwijderd
-{
-    removeFrom.erase(0,3);
-    return removeFrom;
-}
-
-string takeFirstThree(string removeFrom) //Een fuctie waarmee de eerste 3 karakters van een string worden behouden
-{
-    removeFrom.erase(3,removeFrom.size());
-    return removeFrom;
 }
