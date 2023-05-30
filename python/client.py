dcaf37bc78fc4701975b084e3be7579d1cb8e166
Philippe-Arnaud Hiroux <12002903@student.pxl.be>
2023-05-30 12:14:54 +0200

    python full client zmq

--- python/client.py
+++ python/client.py
@@ -2,6 +2,18 @@ import zmq
 import random
 import string
 
+def printGet(input):
+    input = input[5:]
+    input = ''.join([char for char in input if not char.isupper()])
+    input = [substring.strip() for substring in input.split(";")]
+    for elements in input:
+        print("   -> ", elements)
+
+def printOutput(input):
+    input = input[5:]
+    input = ''.join([char for char in input if not char.isupper()])
+    print(input)
+
 shopID = string.ascii_uppercase
 shopID = ''.join(random.choice(shopID) for _ in range(10))
 print(shopID)
@@ -15,13 +27,18 @@ socketRecv.connect("tcp://benternet.pxl-ea-ict.be:24042")
 
 topicRecv = "shop?" + shopID
 topicSend = "shop!" + shopID
+command = ""
 socketRecv.setsockopt_string(zmq.SUBSCRIBE, topicRecv)
 
 while(1):
     messageSend = input("Type in your command: ")
+    messageSend = messageSend.lower()
+    command = messageSend[:3]
     messageSend = topicSend + messageSend
-    print(messageSend)
     socketSend.send_string(messageSend)
     reply = socketRecv.recv_string()
-    print(reply)
+    if(command == "get"):
+        printGet(reply)
+    else:
+        printOutput(reply)
 
